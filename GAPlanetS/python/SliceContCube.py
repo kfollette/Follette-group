051acd35172d7c83026e1dfb3aae2f818399819f
Kate Follette <kfollette@amherst.edu>
2016-09-16 14:06:43 -0700

    adding changes to python scripts

---
+++ GAPlanetS/python/SliceContCube.py
@@ -0,0 +1,36 @@
+#Take in data cube
+#Slice it in to X individual files, print corresponding rotoff parameters to headers from separate file
+
+from astropy.io import fits
+import os
+import numpy as np
+
+print("Enter directory filepath from " + str(os.getcwd()))
+dir = input()
+os.chdir(str(dir))
+hdulist = fits.open("Cont_clip450_flat_reg_circsym_nocosmics.fits")
+cube = hdulist[0].data
+hdulist.close()
+
+hdulist = fits.open("rotoff_nocosmics.fits")
+rotoffs = hdulist[0].data
+hdulist.close()
+
+if not os.path.exists("sliced_Cont/"):
+    os.makedirs("sliced_Cont/")
+
+os.chdir("sliced_Cont/")
+
+for z in range(len(cube)):
+    newFITS = np.zeros((450,450))
+    for y in range(450):
+        for x in range(450):
+            newFITS[y][x] = cube[z][y][x]
+    hdu = fits.PrimaryHDU(newFITS)
+    hdulist = fits.HDUList([hdu])
+    prihdr = hdulist[0].header
+    prihdr.set('rotoff', str(rotoffs[z]))
+    hdulist.writeto("sliced_"+str(z+1)+".fits")
+    print("Sliced continuum image " + str(z+1))
+
+print("Done slicing")

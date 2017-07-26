150ee598e9bbec8675e4bc4db874b2eeb50eb730
Alex Watson <awatson18@amherst.edu>
2017-07-26 15:30:48 -0400

    Update header info and general code for ReSlice

--- ReSlice.py
+++ ReSlice.py
@@ -13,28 +13,43 @@ Last modified: 6/30/2017
 import sys
 from astropy.io import fits
 import numpy as np
+import os
 
-###INPUTS###
-hdulist = fits.open(sys.argv[1])
-Line_data = hdulist[0].data
-header_data = fits.getheader(sys.argv[1],0)
-hdulist.close()
-hdulist = fits.open(sys.argv[2])
-Cont_data = hdulist[0].data
-hdulist.close()
-
-###MAIN CODE###
-size = Line_data.shape[0]
-size2 = Cont_data.shape[0]
-if(not size == size2):
-    sys.exit("Line and Continuum data cubes are not the same size.")
-
-count = 1
-for z in range(size):
-    SL = Line_data[z]
-    SC = Cont_data[z]
-    NewSlice = np.vstack(([SL],[SC]))
-    hdu = fits.PrimaryHDU(NewSlice, header_data)
-    hdulist = fits.HDUList([hdu])
-    hdulist.writeto("Cube_" + str(count) + ".fits", overwrite=True)
-    count += 1
+def slice(linefile, contfile, rotoff):
+    
+    ###INPUTS###
+    hdulist = fits.open(linefile)
+    linecube = hdulist[0].data
+    header_data = fits.getheader(linefile, 0) #will be the same as the cont header
+    hdulist.close()
+    hdulist = fits.open(contfile)
+    contcube = hdulist[0].data
+    hdulist.close()
+    hdulist = fits.open(rotoff)
+    r = hdulist[0].data
+    hdulist.close()
+    
+    ###OUTPUT PATH###
+    if not os.path.exists("sliced"):
+        os.makedirs("sliced")
+    os.chdir("sliced")
+    
+    ###MAIN CODE###
+    size = linecube.shape[0]
+    size2 = contcube.shape[0]
+    size3 = r.shape[0]
+    if(not size == size2 and size == size3):
+        sys.exit("Line, Continuum, and rotoff data are not all the same size.")
+        
+    for z in range(size):
+        SL = linecube[z]
+        SC = contcube[z]
+        NewSlice = np.vstack(([SL],[SC]))
+        hdu = fits.PrimaryHDU(NewSlice, header_data)
+        hdulist = fits.HDUList([hdu])
+        prihdr = hdulist[0].header
+        prihdr.set('rotoff', str(r[z]))
+        hdulist.writeto("Cube_" + str(z+1) + ".fits", overwrite=True)
+        print("Sliced layer " + str(z+1))
+        
+    print("Done slicing.")

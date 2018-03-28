0a0101dc7fd3bfae478869cbed889a09950e313d
Clare Leonard <cleonard19@amherst.edu>
2018-03-28 10:24:55 -0400

    add option to print available directories to synch to s3pull.py

--- s3pull.py
+++ s3pull.py
@@ -4,12 +4,25 @@ import sys
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('ac-follettelab')
-dirname = sys.argv[1]
+try:
+    dirname = sys.argv[1]
+except:
+    dirname = ''
+if(dirname  == 'options'):
+    dirname = ''
+
+dirs = ()
 
 for obj in bucket.objects.filter(Prefix = 'follette-lab/cloud/input/' +dirname + '/'):
     filename = obj.key.rsplit('/')
-    filename = filename[-2] + '/' + filename[-1]
-    if not os.path.exists('/data/' + obj.key.rsplit('/')[-2]):
-        os.makedirs('/data/' + obj.key.rsplit('/')[-2])
-    print(filename)
-    s3.meta.client.download_file('ac-follettelab', obj.key, '/data/' + filename)
+
+    if (dirname == ''):
+        if(filename[-2] not in dirs):
+            print(filename[-2])
+            dirs.append(filename[-2])
+    else:
+        filename = filename[-2] + '/' + filename[-1]
+        if not os.path.exists('/data/' + obj.key.rsplit('/')[-2]):
+            os.makedirs('/data/' + obj.key.rsplit('/')[-2])
+        print(filename)
+        s3.meta.client.download_file('ac-follettelab', obj.key, '/data/' + filename)

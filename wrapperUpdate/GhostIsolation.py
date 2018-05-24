0e47235a350ed5b293102ae4bee651dd8a042157
Katherine Follette <kfollette@amherst.edu>
2018-05-24 11:54:55 -0400

    moving Alex's ghost isolation code to main repo

--- wrapperUpdate/GhostIsolation.py
+++ wrapperUpdate/GhostIsolation.py
@@ -1,204 +1,130 @@
-import os
-import sys
-import math
+#import os
+#import sys
+#import math
 from astropy.io import fits
+from astropy.modeling import models, fitting
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
+#import glob
 from scipy.optimize import curve_fit
 from scipy.integrate import quad
-import pandas as pd
+#import pandas as pd
 from numpy import sqrt
 import image_registration as ir
 
 def readFitsCubeToArray(filename):
-    global data
-    global original
-    hdulist = fits.open(filename)
-    original = hdulist[0].data
-    hdulist.close()
+    original = fits.getdata(filename)
     print("read fits file into data")
     medianImage = np.nanmedian(original, axis=0)
     data = medianImage
+    #fits.writeto("test_median.fits",data,overwrite=True)
     print("created median")
+    return original, data
 
-def findGhostSingleIm():
-    global ghostX
-    global ghostY
-    #count = 0
-    xGhost = 383
-    yGhost = 217
+def findGhostSingleIm(gX,gY, data):
+    """
+    takes gX and gY, guesses for the location of the ghost image
+    returns the actual coordinates of the approximate ghost center 
+    and the pixel value at that point
+    """
+    #xGhost = 383
+    #yGhost = 217
     maxVal = 0
     tempX = 0
     tempY = 0
-    for y in range(yGhost-15, yGhost+16):
-        for x in range(xGhost-15,xGhost+16):
+    
+    #finds center based on brightest pixel around location of ghost
+    for y in range(gY-15, gY+16):
+        for x in range(gX-15,gX+16):
             if data[y,x] > maxVal:
                 tempX = x
                 tempY = y
                 maxVal = data[y,x]
     print("Calculated ghost center is " + str(maxVal) + " at x=" + str(tempX) + " , y=" + str(tempY))
-    ghostX = tempX
-    ghostY = tempY
+    print("returning")
     return (tempX, tempY, maxVal)
 
-def makeSquare():
-    global maskedArray
+def makeSquare(xc,yc,data,filename="test_square.fits"):
+    foundX, foundY, maxVal = findGhostSingleIm(xc,yc,data)
     maskedArray = np.zeros((31,31))
     for y in range(-15,16):
         for x in range(-15,16):
-            maskedArray[y+15][x+15] = data[ghostY+y][ghostX+x]
+            maskedArray[y+15][x+15] = data[foundY+y][foundX+x]
+    fits.writeto(filename, maskedArray, overwrite=True)
+    return maskedArray, foundX, foundY
 
-def isolateGhostArray():
-     center = []
-     for x in range (0, 30):
-         value = maskedArray[15,x]
-         center.append(value)
-     return center
- 
-def isolateGhostArray2():
-    center = []
-    for y in range (0, 30):
-        value = maskedArray[y,15]
-        center.append(value)
-    return center
+def moffat(xcen,ycen,amp,wid,power):
+    return models.Moffat2D(amplitude=amp, x_0=xcen, y_0=ycen, gamma=wid, alpha=power)
 
-def modifyDataSet():
-    y = data
-    length = len(y)
-    x = list(range(len(y)))
-    i = 0 
-    while i < (len(x)):
-        if (y[i]>15500 or x[i]== np.nan):
-            x.remove(x[i])
-            y.remove(y[i])
-        else:
-            i = i+1
-    if (len(y) == length):
-        return x,y, 'unsat'
-    else:
-        return x, y, 'sat'
-
-def moffat(x, amp, cen, wid, pow):
-    return (amp*(((x-cen)/wid)**(2)+1)**(-pow))
-
-def makeMoffatGhost(array):
+def makeMoffatGhost(array,xcen,ycen,amp,wid,power):
     global best_vals2
-    try:
-        #an array of x values corresponding to the size of the array
-        new_x = list(range(len(array)))
+    y,x = np.mgrid[:31,:31]
+    z = array
+    #xcen,ycen,mv = findGhostSingleIm(gx,gy)
+    model = moffat(xcen,ycen,amp,wid,power)
+    print(model.x_0)
+    lfit = fitting.LevMarLSQFitter()
+    fit = lfit(model, x, y, z)
+    '''
+        plt.figure(figsize=(8, 2.5))
+        plt.subplot(1, 3, 1)
+        plt.imshow(z, origin='lower', interpolation='nearest', vmin=-1e4, vmax=5e4)
+        plt.title("Data")
+        plt.subplot(1, 3, 2)
+        plt.imshow(fit(x, y), origin='lower', interpolation='nearest', vmin=-1e4, vmax=5e4)
+        plt.title("Model")
+        plt.subplot(1, 3, 3)
+        plt.imshow(z - fit(x, y), origin='lower', interpolation='nearest', vmin=-1e4, vmax=5e4)
+        plt.title("Residual")
+    '''
+    print("x center is " + str(fit.x_0))
+    print("y center is " + str(fit.y_0))
+    return fit.x_0, fit.y_0
 
-        #uses the ghost array as y values
-        new_y = array
-
-        # initial (dumb) guess for [amp, cen, wid] of moffat
-        init_vals = [1, 15, 10, 0.5]
-        #curvefit spits out two arrays - one with the fit parameters (best_vals) and one with the covariance matrix (covar1)
-        best_vals1, covar1 = curve_fit(moffat, new_x, new_y, p0=init_vals)
-        
-        #add noise
-        #read noise
-        rd_noise = np.repeat(0.02, len(new_x))
-
-        #photon noise
-        #just so that there are no negative values, add a constant. this is an oversimplification, but will work for now.
-        floor=np.repeat(abs(min(new_y)),len(new_x)) 
-        pt_noise = sqrt(new_y+floor) #add me - /sqrt(nimages)
-        #noise adds in quadrature
-        e = sqrt(rd_noise**2 + pt_noise**2)
-        #plt.errorbar(new_x, new_y, yerr=e, fmt='none')
-        best_vals2, covar2 = curve_fit(moffat, new_x, new_y, p0=init_vals, sigma=e)
-        #best_vals2
-
-
-        print("amp =", best_vals2[0], "+/-", covar2[0,0]**0.5)
-        print ("cen =", best_vals2[1], "+/-", covar2[1,1]**0.5)
-        print ("wid =", best_vals2[2], "+/-", covar2[2,2]**0.5)
-        plt.figure(1)
-        plt.errorbar(new_x, new_y, yerr=e, fmt='none', ecolor = 'r')
-        #np.linspace?
-        xfine = np.linspace(0., 30., 30000)  # define values to plot the fit for
-        #plt.plot(xfine, gaussian(xfine, best_vals2[0], best_vals2[1], best_vals2[2]), 'r-')
-        plt.plot(xfine, moffat(xfine, *best_vals2), 'c-')
-        plt.xlabel('Pixel Number')
-        plt.ylabel('Pixel Value')
-        plt.legend(('moffat best fit', 'data'), loc='upper left')
-        plt.show()
-        plt.close()
-        #plt.title("%s %s ghost" %(name, date))
-        print(str(best_vals2))
-        #plt.savefig('%s_%s_ghost.png' %(name,date))
-        #plt.clf()
-
-        def moff(x):
-            return moffat(x, *best_vals2)
-        
-        area, areaErr = quad(moff, 0, 119)
-        amp = best_vals2[0]
-        ampErr = covar2[0,0]
-        print("the area under the curve is", str(area))
-        wid = best_vals2[2]
-        widErr = covar2[2,2]
-        #ghostPeaks.append(amp)
-        return amp, ampErr, wid, widErr, area, areaErr
-
-    except (RuntimeError):
-            print ('signal/noise too low; no optimal fit found')
-            plt.figure(2)
-            xfine = np.linspace(0., 120., 30000)  # define values to plot the fit for
-            #plt.plot(xfine, gaussian(xfine, best_vals2[0], best_vals2[1], best_vals2[2]), 'r-')
-            plt.xlabel('Pixel Number')
-            plt.ylabel('Pixel Value')
-            plt.legend(('moffat best fit', 'data'), loc='upper left')
-            #plt.title("No Star Fit for %s %s" %(name, date))
-            #plt.savefig('%s_%s_ghost.png' %(name,date))
-
-            plt.clf()
-
-            return 'no fit', 'no fit','no fit','no fit','no fit','no fit'
-
-def normalizeSquare():
+def normalizeSquare(array):
     maxVal = 0
     for y in range(31):
         for x in range(31):
-            if maskedArray[y][x] > maxVal:
-                maxVal = maskedArray[y][x]
+            if array[y][x] > maxVal:
+                maxVal = array[y][x]
     for y in range(31):
         for x in range(31):
-            maskedArray[y][x] = maskedArray[y][x] / maxVal
+            array[y][x] = array[y][x] / maxVal
             
-def shift(x,y):
+def shift(x,y,original):
     size = original.shape[0]
+    shiftX, shiftY = x-15, y-15
     for z in range(size):
-        shiftX, shiftY = x-15, y-15
         shifted_image = ir.fft_tools.shift2d(original[z],-shiftX,-shiftY)
         original[z] = shifted_image
         if(z%100 == 0):
-            print("shift number " + str(z))    
+            print("shift number " + str(z))  
 
 ################
 
-readFitsCubeToArray("HD142527_8Apr14_Line_clip450_flat_reg_circsym.fits")
-findGhostSingleIm()
-makeSquare()
-amp1, ampErr1, wid1, widErr1, area1, areaErr1 = makeMoffatGhost(isolateGhostArray())
-centerX = best_vals2[1]
-amp2, ampErr2, wid2, widErr2, area2, areaErr2 = makeMoffatGhost(isolateGhostArray2())
-centerY = best_vals2[1]
+def ghostIsolation(filename, gx, gy, amp, wid, power):
 
-shift(centerX,centerY)
+    original, data = readFitsCubeToArray(filename)
+    maskedArray, foundX, foundY = makeSquare(gx,gy,data)
 
-medianImage = np.nanmedian(original, axis=0)
-data = medianImage
-print("created median")
-makeSquare()
-normalizeSquare()
-hdu = fits.PrimaryHDU(maskedArray)
-hduList = fits.HDUList([hdu])
-hduList.writeto("test.fits", overwrite=True)
+    #convert to maskedArray's coordinates
+    foundXSmall = foundX+15-gx
+    foundYSmall = foundY+15-gy
 
-'''
-with open("moffat.txt","w") as text_file:
-    text_file.write(str(amp) + " " + str(ampErr) + " " + str(wid) + " " + str(widErr) + " " + str(area) + " " + str(areaErr))
-    text_file.close()
-'''
+    xCenter, yCenter = makeMoffatGhost(maskedArray,foundXSmall,foundYSmall,amp,wid,power)
+    print(xCenter, yCenter)
+
+    shift(xCenter,yCenter, original)
+
+    medianImage = np.nanmedian(original, axis=0)
+    data = medianImage
+    print("created median after shifting")
+    maskedArray2, a, b = makeSquare(foundX,foundY,data,"ghost.fits")
+    #normalizeSquare(maskedArray2)
+    #fits.writeto("test_final.fits", maskedArray2, overwrite=True)
+    
+    '''
+    with open("moffat.txt","w") as text_file:
+        text_file.write(str(amp) + " " + str(ampErr) + " " + str(wid) + " " + str(widErr) + " " + str(area) + " " + str(areaErr))
+        text_file.close()
+    '''

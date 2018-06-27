fe5e13da8d3791b8bb5037ff229befc02e6c9d9e
Katherine Follette <kfollette@amherst.edu>
2018-06-27 10:46:11 -0400

    SDI parameter exploration tests

--- wrapperUpdate/SDI_paramexplore.py
+++ wrapperUpdate/SDI_paramexplore.py
@@ -4,28 +4,33 @@
 import numpy as np
 from astropy.io import fits
 
-annuli = [1,2]
-movement = [1.,2.]
-#annuli = list(np.arange(0,36))
-#movement = list(np.arange(0,23))
+#annuli = [1,2,3,4,5,6]
+#movement = [1,2,3,4,5,6,7,8,9,10,11,12]
+annuli = list(np.arange(1,31))
+movement = list(np.arange(0,23))
 subsections = [1]
 iwa = [0]
-scale=[1]
+scale=[1.6]
 klmodes = [1,2,5,10,20,50,100]
-filename = 'LkCa15_18Nov16'
-filepath='/Users/kfollette/Shared_Dropbox/Follette-Lab/GAPlanetS_Data/GAPlanetS_2017Reprocessing/LkCa15/18Nov16/merged/fwhm38cut/' 
+linefilename = 'LkCa15_16Nov14_Line_1e2fake_nobpl'
+contfilename = 'LkCa15_16Nov14_Cont'
+#filepath='/Users/kfollette/Shared_Dropbox/Follette-Lab/GAPlanetS_Data/GAPlanetS_2017Reprocessing/LkCa15/18Nov16/merged/fwhm38cut/' 
 #filename='LkCa15_16Nov14_1e1fake_nobpl'
 #filepath='/Users/kfollette/Shared_Dropbox/Follette-Lab/GAPlanetS_Data/GAPlanetS_2017Reprocessing/LkCa15/16Nov14/wfe171pt9/'
+filepath='/Users/kfollette/Shared_Dropbox/Follette-Lab/GAPlanetS_Data/GAPlanetS_2017Reprocessing/LkCa15/KLIP_experiments/'
+linedir='lkca15_16nov14_sliced_line_1e2fake_pa100_nobpl_KLIP'
+contdir='lkca15_16nov14_sliced_cont_KLIP'
+outdir='SDI_1e2pl_pa100_KLIP/'
 
 for a in np.arange(len(annuli)):
     for m in np.arange(len(movement)):
         for s in np.arange(len(subsections)):
             print(a,m,s)
-            line = fits.getdata(str(filepath)+'sliced_Line_KLIP/' +'med_'  + filename +'_Line'+ '_a' + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0])  +  '_KLmodes-all.fits', clobber=True)
-            hdr = fits.getheader(str(filepath)+'sliced_Line_KLIP/' +'med_'  + filename +'_Line'+ '_a' + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0])  +  '_KLmodes-all.fits', clobber=True)
-            cont=fits.getdata(str(filepath)+'sliced_Cont_KLIP/' +'med_'  + filename +'_Cont'+ "_a" + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0])  +  '_KLmodes-all.fits', clobber=True)
+            line = fits.getdata(str(filepath)+linedir +'/med_'  + linefilename + '_a' + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0])  +  '_KLmodes-all.fits', clobber=True)
+            hdr = fits.getheader(str(filepath)+linedir +'/med_'  + linefilename + '_a' + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0])  +  '_KLmodes-all.fits', clobber=True)
+            cont=fits.getdata(str(filepath)+contdir +'/med_'  + contfilename + "_a" + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0])  +  '_KLmodes-all.fits', clobber=True)
             SDI = line - scale*cont
     #        hdu = fits.PrimaryHDU(SDI)
      #       hdulist=fits.HDUList([hdu])
-            fits.writeto(str(filepath)+'sliced_SDI_KLIP/med_SDI'+"_a" + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0]) +'_KLmodes-all.fits', SDI, hdr, clobber=True) 
+            fits.writeto(str(filepath)+str(outdir)+'med_SDI'+"_a" + str(annuli[a]) + "m" + str("{:.1f}".format(movement[m])) + "s" + str(subsections[s]) + "iwa" + str(iwa[0]) +'_KLmodes-all.fits', SDI, hdr, clobber=True) 
 

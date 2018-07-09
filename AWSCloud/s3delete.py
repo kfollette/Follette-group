e749b33b1e2316dda0c50bca90ae521fffeaaa68
Clare Leonard <cleonard19@amherst.edu>
2018-07-09 11:21:36 -0400

    Add except clause to s3delete.py to avoid error

--- AWSCloud/s3delete.py
+++ AWSCloud/s3delete.py
@@ -9,8 +9,10 @@ objects_to_delete = []
 for obj in bucket.objects.filter(Prefix='follette-lab/cloud/input/' +dirname + '/'):
     objects_to_delete.append({'Key': obj.key})
 
-bucket.delete_objects(
-    Delete={
-        'Objects': objects_to_delete
-    }
-)
+
+try:
+    bucket.delete_objects(
+        Delete={'Objects': objects_to_delete})
+except:
+    for obj in bucket.objects.filter(Prefix='follette-lab/cloud/input/' +dirname + '/'):
+        bucket.delete_key(obj.key)

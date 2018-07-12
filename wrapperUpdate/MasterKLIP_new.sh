2ebf5968669d989e86238fbe062e40da00d5f884
Clare Leonard <cleonard19@amherst.edu>
2018-07-12 16:46:59 -0400

    Debug new MasterKLIP script

--- wrapperUpdate/MasterKLIP_new.sh
+++ wrapperUpdate/MasterKLIP_new.sh
@@ -1,6 +1,7 @@
 #Elijah Spiro
 #Version 2.1 - 3/26/17
 #edited by Clare Leonard - 8/1/2017
+#Version 3.1 - 7/12/2018
 
 #Increase limit on maximum open files
 ulimit -n 4096
@@ -18,83 +19,16 @@ rm *.class
 if [ -f single_reduction_parameters.txt ]; then
     echo "Performing single reduction"
     echo ""
-    RUNTYPE=1
+    python RunKLIP.py `< single_reduction_parameters.txt`
+    rm single_reduction_parameters.txt
 fi
 if [ -f automation_parameters.txt ]; then
     echo "Performing automation"
     echo ""
-    RUNTYPE=2
+    python AutomateKLIP.py `< automation_parameters.txt`
+    rm automation_parameters.txt
 fi
 
-#Read in parameters from one of the two parameter files
-if [[ $RUNTYPE = 2 ]]; then
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-        ((counter+=1))
-        if [ $counter = 1 ]; then
-            FILEPATH=$line
-        elif [ $counter = 2 ]; then
-            IWA=$line
-        elif [ $counter = 3 ]; then
-            KLMODES=$line
-        elif [ $counter = 4 ]; then
-            OUTPUTNAME=$line
-        elif [ $counter = 5 ]; then
-            A1=$line
-        elif [ $counter = 6 ]; then
-            A2=$line
-        elif [ $counter = 7 ]; then
-            A3=$line
-        elif [ $counter = 8 ]; then
-            M1=$line
-        elif [ $counter = 9 ]; then
-            M2=$line
-        elif [ $counter = 10 ]; then
-            M3=$line
-	elif [ $counter = 11 ]; then
-            S1=$line
-        elif [ $counter = 12 ]; then
-            S2=$line
-	elif [ $counter = 13 ]; then
-            S3=$line
-	elif [ $counter = 14 ]; then
-            FWHM=$line
-    elif [ $counter = 15 ]; then
-            SMOOTH=$line
-	elif [ $counter = 16 ]; then
-            ra=$line
-	elif [ $counter = 17 ]; then
-            pa=$line
-	elif [ $counter = 18 ]; then
-            wi=$line
-	elif [ $counter = 19 ]; then
-            SAVE2=$line
-        fi
-    done < "automation_parameters.txt"
-fi
-
-
-#Run StartKLIP.py with command line arguments from these variables (if single reduction)
-if [[ $RUNTYPE = 1 ]]; then
-    python RunKLIP.py < single_reduction_parameters.txt
-    #/home/anaconda3/bin/python3 StartKLIP.py $FILEPATH $ANNULI $IWA $MOVEMENT $OUTPUTNAME $KLMODES $SUBSECTIONS $SNR
-    #rm $OUTPUTNAME"-KLmodes-all.fits"
-    #cp "_"$OUTPUTNAME"-KLmodes-all.fits" $OUTPUTNAME".fits"
-    #rm "_"$OUTPUTNAME"-KLmodes-all.fits"
-    #cp $OUTPUTNAME".fits" "temp_klip.fits"
-    #rm temp_klip.fits
-else 
-    python AutomateKLIP.py $FILEPATH $IWA $KLMODES $OUTPUTNAME $A1 $A2 $A3 $M1 $M2 $M3 $S1 $S2 $S3 $FWHM $SMOOTH $ra $pa $wi $SAVE2
-fi
-
-
-     
-if [ -f "single_reduction_parameters.txt" ]; then
-   rm single_reduction_parameters.txt
-fi
-
-if [ -f "automation_parameters.txt" ]; then
-   rm automation_parameters.txt
-fi
 
 if [ -f "single_reduction_parameters.txt~" ]; then
    rm single_reduction_parameters.txt~

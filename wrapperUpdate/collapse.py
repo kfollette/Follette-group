23602612cc990aaaef59a9a9ac90ea199d1aa122
Clare Leonard <cleonard19@amherst.edu>
2018-11-28 13:42:59 -0500

    Add collapse.py to collapse parameter explorer in kl mode dimension

---
+++ wrapperUpdate/collapse.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+from astropy.io import fits
+
+filename = sys.argv[1]
+kl = list(map(int, sys.argv[2].split(",")))
+file = fits.getdata(filename)
+all_kl = list(map(int, fits.open(filename)[0].header['KLMODES'][1:-1].split(",")))
+
+collapsed = np.zeros((1, file.shape[2], file.shape[3]))
+for i in kl:
+    collapsed += file[all_kl.index(i)]
+
+collapsed = collapsed/len(kl)
+hdu = fits.PrimaryHDU(collapsed)
+hdulist = fits.HDUList([hdu])
+hdulist.writeto(filename[:-5] + "_collapsed.fits", overwrite=True)

da1b3b0cc7a499c4b99649d25519d6e78841d115
kthanawala21 <47213046+kthanawala21@users.noreply.github.com>
2019-07-25 11:13:42 -0400

    Add files via upload

--- MagAOX/front_end/forms.py
+++ MagAOX/front_end/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, FloatField, SubmitField
 from wtforms.validators import InputRequired
 
 
 class targetform(FlaskForm):
-    targets = StringField('Targets',validators=[InputRequired()])
-    #need to validate entry - query check?
+    targets = StringField('Targets', validators=[InputRequired()])
+    starttime = StringField('Start Time', validators=[InputRequired()])
+    endtime = StringField('End Time', validators=[InputRequired()])
+    mode = StringField('Instrument Rotator Mode', validators=[InputRequired()])
+    angle = FloatField('Instrument Rotator Angle', validators=[InputRequired()])
+    guide1 = StringField('Guide Star 1', validators=[InputRequired()])
+    guide2 = StringField('Guide Star 2', validators=[InputRequired()])
+    pmepoch = FloatField('PM Epoch', validators=[InputRequired()])
     submit = SubmitField('Submit')                      
--- MagAOX/front_end/main.py
+++ MagAOX/front_end/main.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import targetform
+import catalog_entry
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'magaox_follettelab'
 
 @app.route("/", methods=['GET', 'POST'])
 def loadtargetform():
-    return render_template('targetform.html', form=targetform())
+    form = targetform()
+    if form.validate_on_submit():
+        flash(f'Loading target information for {form.targets.data}!', 'success')
+        return redirect(url_for('loadlandingpage'))
+    return render_template('targetform.html', title='Target Form', form=form)
+
+@app.route("/landingpage")
+def loadlandingpage():
+    return render_template('landingpage.html', title='Results')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

69b8a528603a35241221636e946970427c0bf81e
rvaninmunoz22 <60942982+rvaninmunoz22@users.noreply.github.com>
2020-07-02 15:11:59 -0400

    Create catalog.py

---
+++ MagAOX/webapp/catalog.py
@@ -0,0 +1,84 @@
+import numpy as np, pandas as pd, scipy.stats as st, matplotlib.pyplot as plt
+#get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib
+matplotlib.use('Agg')
+from astroquery.simbad import Simbad
+from astropy.io import ascii
+from astropy import constants as const
+from astropy.time import Time
+from astropy import units as u
+from astropy.coordinates import *
+from astropy.modeling.blackbody import blackbody_lambda, blackbody_nu
+from astropy.table import Table
+from astroplan import (AltitudeConstraint, AirmassConstraint, AtNightConstraint)
+from astroplan import FixedTarget, Observer
+from astroplan import is_observable, is_always_observable, months_observable
+from astroplan import observability_table
+from astroplan import FixedTarget, Observer
+from astropy.coordinates import EarthLocation
+from astroplan.plots import plot_airmass
+from astroplan.plots import plot_sky
+from astropy.time import Time
+import scipy.optimize as optimization
+from scipy.optimize import curve_fit
+from scipy.stats import linregress
+from sklearn.neighbors import KernelDensity
+from subprocess import *
+import matplotlib.image as mpimg
+
+
+import sys
+import numpy as np
+import pandas as pd
+from astroquery.simbad import Simbad
+from subprocess import *
+from flask import Flask
+from flask import request, redirect, render_template
+from call import create #change import call to "from call.py import create"
+from Visibility import plot, vis
+from helper import *
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #attempts to prevent caching
+
+#@app.route("/test2", methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
+def server():
+
+    if request.method == "POST":
+
+        req = request.form
+
+
+        objects = req.get("Objects")
+        mode = req["Mode"]  #change req["mode"] to req.get("Objects")
+        offset = request.form["Offset"]
+        end = req.get("end_Date")
+        start = req.get("start_Date")
+
+
+        create(objects,mode,offset) #change call(objects,mode,offset) to create(objects,mode,offset)
+        plot(start,end,objects)
+        vis(start, end, objects)
+
+        #img = mpimg.imread('static/airmass.png')
+        with open("catalog.txt", "r") as f:
+            content = f.read()
+        table = txt_to_df('catalog.txt')
+        
+        print(table)
+        
+        visib = txt_to_df('visibility.txt')
+        print(visib)
+        #return render_template("log.html", text=content, IM=img)
+        return render_template("log.html", text=content, t1=table, t2=visib)
+
+    else:
+        return render_template("test2.html")
+
+@app.route('/test2.html') #if home if spressed, render home template
+def home():
+    return render_template("test2.html")
+
+if __name__ == "__main__":
+    app.run()

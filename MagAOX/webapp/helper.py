f6cce1621683da7d98a3c07031c6e76ad3e0add7
rvaninmunoz22 <60942982+rvaninmunoz22@users.noreply.github.com>
2020-07-05 16:46:00 -0400

    Update helper.py

--- MagAOX/webapp/helper.py
+++ MagAOX/webapp/helper.py
@@ -5,24 +5,34 @@ def txt_to_df(filename):
 	str = cat.read()
 	
 	li = list(str.replace('\n', '  ').split('  '))
-	newList = [li[0], li[1]]
-	for i in range(2, len(li)):
+	#newList = [li[0], li[1]]
+	newList = []
+	for i in range(len(li)):
 		if not li[i] == '':
 			newList.append(li[i])
 	li = newList
 	print(li)
 	
+	if filename == 'catalog.txt':
+		column = 16
+	
+	if filename == 'visibility.txt':
+		column = 4
+	
+	return makeTable(li,column)
+	
+def makeTable(li,n):	
 	dict = {}
-	for i in range(0, 16):
+	for i in range(0, n):
 		dict[li[i]] = []
 	
 	x = 0
 	for column in dict:
-		for i in range(16+x, len(li), 16):
+		for i in range(n+x, len(li), n):
 			dict[column].append(li[i])
 		if ' ' in dict[column]:
 			dict[column].remove(' ')
 		x+=1
 	
 	table = pd.DataFrame(dict)
-	return table.to_html(index = False)
+	return table

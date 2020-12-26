8ab4d65d481b9b3229f0ac761a33d9671a4d8765
Kate Follette <kfollette@amherst.edu>
2020-12-26 13:26:36 -0500

    Bug fix. Sliced images being written out 31x31 for non-saturated images

--- GAPlanetS/python/pyklip_headers.py
+++ GAPlanetS/python/pyklip_headers.py
@@ -80,7 +80,7 @@ def addstarpeak(dir, debug=False, mask=False, ghost=False, wl='Line'):
         if ghost == True:
             fits.writeto(filelist[i], imcopy, header=head, overwrite=True)
         else:
-            fits.writeto(filelist[i], im, header=head, overwrite=True)
+            fits.writeto(filelist[i], imcopy, header=head, overwrite=True)
 
         if debug == True:
             # print(filelist[i])

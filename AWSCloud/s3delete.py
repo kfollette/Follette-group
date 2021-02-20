3032af70e46907f5c7f1ce2c6cf92e82c65aedfb
Kate Follette <kfollette@amherst.edu>
2021-02-19 20:10:16 -0500

    add delete all option

--- AWSCloud/s3delete.py
+++ AWSCloud/s3delete.py
@@ -1,18 +1,27 @@
 import boto3
 import sys
+import glob
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('amherst-follette-lab')
 dirname = sys.argv[1]
 
-objects_to_delete = []
-for obj in bucket.objects.filter(Prefix='follette-lab/cloud/input/' +dirname + '/'):
-    objects_to_delete.append({'Key': obj.key})
+if dirname=='all'
+	go = input('Are you sure you would like to delete everything in the s3? (y/n)')
+	if go=='y' or go=='Y':
+		bucket.object_versions.delete()
+	else:
+		print('ok. doing nothing.')
+
+else:
+	objects_to_delete = []
+	for obj in bucket.objects.filter(Prefix='follette-lab/cloud/input/' +dirname + '/'):
+	    objects_to_delete.append({'Key': obj.key})
 
 
-try:
-    bucket.delete_objects(
-        Delete={'Objects': objects_to_delete})
-except:
-    for obj in bucket.objects.filter(Prefix='follette-lab/cloud/input/' +dirname + '/'):
-        obj.delete()
+	try:
+	    bucket.delete_objects(
+	        Delete={'Objects': objects_to_delete})
+	except:
+	    for obj in bucket.objects.filter(Prefix='follette-lab/cloud/input/' +dirname + '/'):
+	        obj.delete()

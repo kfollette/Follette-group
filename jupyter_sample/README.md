53b944cd5151f0d8061e5dfc3cf7689243529d37
Kate Follette <kfollette@amherst.edu>
2021-03-19 13:40:09 -0400

    general repo cleanup - removing obsolete files, moving some stragglers to appropriate directories

--- jupyter_sample/README.md
+++
@@ -1,4 +0,0 @@
-# astronomy_camp_tutorials
-
-Python 3 notebooks to introduce programming and astronomical data analysis for Astronomy Camp. 
-http://www.astronomycamp.org
--- pyKLIP_edits/runKLIP_scripts.sh
+++
@@ -1,6 +0,0 @@
-ulimit -n 4096
-
-python RunKLIP.py /Users/follettelab/Shared_Dropbox/HD142527/8Apr14/merged_short_newest_rerun/Line_sliced 6 1,5,10,20,50,100 3 13 1 pyklip_version3_Line true false 5 13 120 10,15
-
-python RunKLIP.py /Users/follettelab/Shared_Dropbox/HD142527/8Apr14/merged_short_newest_rerun/Cont_sliced 6 1,5,10,20,50,100 3 13 1 pyklip_version3_Cont true false 5 13 120 10,15
-

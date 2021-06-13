f03022f0ce4491be6bd6ab44423dd877db2d4595
Kate Follette <kfollette@amherst.edu>
2021-06-13 12:57:59 -0400

    diagnosing bug - add print statement and pass writestr

--- GAPlanetS/python/peproc.py
+++ GAPlanetS/python/peproc.py
@@ -41,6 +41,7 @@ def collapse_planets(pename, pedir='./', outdir='proc/', writestr=False, snrthre
 	if writestr == False:
 		#use the parameter explorer name for this file as well, minus the '_highpass_klmodes-all.fits'
 		writestr = pename[:-17]
+		print(writestr)
 
 	# read in image and header
 	pecube = fits.getdata(pedir + pename)
@@ -251,7 +252,7 @@ def find_best_new(pename, kllist, pedir='./', writestr=False, weights=[1,1,1,1,1
 		print("COLLAPSING IN PLANET DIMENSION")
 	else:
 		print("EXTRACTING PLANETS SEPARATELY")
-	pecube, writename, npldim = collapse_planets(pename, pedir=pedir, outdir=outdir, snrthresh=snrthresh, oldpe=oldpe, separate_planets=separate_planets)
+	pecube, writename, npldim = collapse_planets(pename, pedir=pedir, outdir=outdir, snrthresh=snrthresh, oldpe=oldpe, writestr=writestr, separate_planets=separate_planets)
 
 	#EXTRACT KL MODES OR COLLAPSE
 	print("EXTRACTING ONLY KL MODES SPECIFIED")

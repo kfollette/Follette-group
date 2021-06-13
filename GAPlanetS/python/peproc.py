bug fix - give metric cube planet and kl dimensions and move definition outside loop

GAPlanetS/python/peproc.py
@@ -308,6 +308,7 @@ def find_best_new(pename, kllist, pedir='./', writestr=False, weights=[1,1,1,1,1
 	agg=np.zeros([len(kllist),npldim,nstepy,nstepx])
 	ann_val = np.zeros([len(kllist),npldim])
 	movm_val = np.zeros([len(kllist),npldim])
+	metric_cube = np.zeros([9,1,klloop npldim, nstepy,nstepx])
 
 	#set up kllist argument for loop
 	if len(kllist)>1 and separate_kls==False:
@@ -404,7 +405,6 @@ def find_best_new(pename, kllist, pedir='./', writestr=False, weights=[1,1,1,1,1
 			#nq_stdev_combo = (nq_stdev + nq_stdev_umask) / 2.
 
 			#write out the cubes being used for the final metric
-			metric_cube = np.zeros([9,nstepy,nstepx])
 			metric_cube[0,:,k,p,:,:]= snr_norm
 			metric_cube[1,:,k,p,:,:]= nq_snr
 			metric_cube[2,:,k,p,:,:]= snr_norm_umask